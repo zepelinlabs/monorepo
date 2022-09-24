8a9484e7eb89001dd777857461c2b4438740de48
Alvaro <26382770+ohduran@users.noreply.github.com>
2022-09-24 13:08:37 +0200

    Add prose style to blogposts (#15)

--- landing/pages/blog/[id].js
+++ landing/pages/blog/[id].js
@@ -1,17 +1,48 @@
-import Head from 'next/head';
 import React from 'react';
 import { getAllPostIds, getPostData } from '../../lib/blogs';
+import Head from 'next/head';
+import NavBar from '../../components/NavBar';
+import SubscribeForm from '../../components/SubscribeForm';
+import styles from '../../styles/blogpost.module.css';
 
 export default function Post({ postData }) {
   return (
-    <div>
+    <div className="bg-gradient-to-br from-nord-6 via-nord-5 to-nord-4">
       <Head>
         <title> Zepelín Labs | {postData.title} </title>
       </Head>
-      <article>
-        <h1>{postData.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article className="min-h-screen">
+        <NavBar />
+        <div className="px-4 sm:px-6 md:px-8">
+          <div className="max-w-3xl mx-auto">
+            <main className="mt-12 prose lg:prose-xl prose-slate">
+              <h1 className="text-2xl font-extrabold tracking-tight text-slate-900 md:text-3xl">
+                {postData.title}
+              </h1>
+              <div
+                className={styles.blogpost}
+                dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+              />
+            </main>
+          </div>
+        </div>
+        <section className="mt-4 sm:mt-8 px-4 sm:px-6 lg:px-8 mx-auto w-full max-w-container grid grid-cols-1">
+          <div className="col-start-1 row-start-3 max-w-2xl mx-auto bg-nord-1 mt-5 pb-5 rounded-lg text-nord-6">
+            <div className="p-4">
+              <h3 className="font-bold text-lg sm:text-lg text-nord-13">
+                Entérate de las últimas novedades un poco más rápido
+              </h3>
+              <p className="font-semibold mt-2">
+                Escribo en torno a una vez por semana, sobre el impacto de la
+                programación en las áreas no tecnológicas de las empresas. Es
+                gratis.
+              </p>
+              <SubscribeForm />
+            </div>
+          </div>
+        </section>
       </article>
+      <footer className="pt-5"></footer>
     </div>
   );
 }
